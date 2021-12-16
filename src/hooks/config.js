533255c528950ec14bd77628965b21f640869a4b
xiaoliao2021 <1137667185@qq.com>
2021-12-17 07:48:37 +0800

    debug凭证

--- src/hooks/config.js
+++ src/hooks/config.js
@@ -2,7 +2,7 @@ import { reactive } from "vue";
 const github = reactive({
   deployment_ciphertext:
     "icM7o2Up7ZDP73FJdAdGtRwtC7kEDI5UYeYwVmYwlpZMj2YC1NM9IKFJFVSEOgRg", //部署令牌密文
-  read_only_auth: "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", //只读令牌
+  read_only_auth: "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", //只读令牌
   repo: "myblog_demo", //项目名称
   ref: "notes", //项目分支
   note_root_path: "note_root", //笔记目录
